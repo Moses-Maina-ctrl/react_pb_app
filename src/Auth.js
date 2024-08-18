08846ff2bda4a29d9a2a6ed11f93b84bb26cc326
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-08-18 23:47:04 +0300

    Create login form

---
+++ src/Auth.js
@@ -0,0 +1,17 @@
+import pb from "lib/pocketbase";
+
+export default function Auth(){
+    return(
+    <>
+    <h1>
+        Logged In: {pb.authStore.isValid.toString()};
+        <form>
+            <input type="text" placeholder="email"/>
+            <input type="password" placeholder="password"/>
+
+            <button type="submit">Login</button>
+        </form>
+    </h1>
+    </>
+    )
+}
